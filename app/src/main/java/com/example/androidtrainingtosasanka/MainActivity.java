b01dcf0d75b1906426d31709583c6c15b13f6948
Nagaraj1027 <nagaraj.pambhala@gmail.com>
2019-01-18 08:04:14 +0530

    First commit

---
+++ app/src/main/java/com/example/androidtrainingtosasanka/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidtrainingtosasanka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
